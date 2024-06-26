743e8d0b97eeb09f3809005607e055c554727ad3
Yash Kumar <Yashxhere@gmail.com>
2024-06-27 03:09:50 +0530

    Minor change

--- Variables.c
+++ Variables.c
@@ -11,5 +11,8 @@ int main()
     printf("a * b = %d\n", a*b);
     printf("a / b = %d\n", a/b);
     printf("a reminders b = %d\n", a%b);
+
+    float pi = 3.14, g = 9.81;
+    printf("A PIG without G(%f) is Pi(%f)\n", g, pi);
     return 0;
 }
