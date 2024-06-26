a0279b310171767a19452da1d02d9a95ab8afa9d
Yash Kumar <Yashxhere@gmail.com>
2024-06-27 02:56:31 +0530

    Changes

---
+++ Variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int x = 250;
+    x += 5;
+    printf("x = %d\n", x);
+    
+    int a = 9, b = 10;
+    printf("a + b = %d\n", a+b);
+    printf("a - b = %d\n", a-b);
+    printf("a * b = %d\n", a*b);
+    printf("a / b = %d\n", a/b);
+    printf("a reminders b = %d\n", a%b);
+    return 0;
+}
